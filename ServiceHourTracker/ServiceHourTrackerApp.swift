de96dbd6cd007345ad80a21bfb517f80fc24361f
AravNeroth <reacharav@outlook.com>
2023-12-05 15:32:26 -0600

    moved files, ready for coding

---
+++ ServiceHourTracker/ServiceHourTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceHourTrackerApp.swift
+//  ServiceHourTracker
+//
+//  Created by neroth_927927 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ServiceHourTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
