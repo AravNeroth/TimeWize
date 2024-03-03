bae856a9ee6569e4a2aa00ba200ed6f5855497fa
parkour557 <pjhuang999@gmail.com>
2024-03-03 14:01:05 -0600

    created class component view

---
+++ ServiceHourTracker/UIElements/ClassComponentView.swift
@@ -0,0 +1,21 @@
+//
+//  ClassComponentView.swift
+//  ServiceHourTracker
+//
+//  Created by huang_931310 on 3/3/24.
+//
+
+import SwiftUI
+
+struct ClassComponentView: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClassComponentView()
+}
