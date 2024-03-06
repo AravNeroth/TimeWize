7885b6a4fb9cfeca18ef5f1b9086062eb9085a75
AravNeroth <reacharav@outlook.com>
2024-03-05 23:46:16 -0600

    dumbahh v stack manager view bug fixed smh

--- ServiceHourTracker/Manager Views/ManagerPeopleView.swift
+++ ServiceHourTracker/Manager Views/ManagerPeopleView.swift
@@ -45,150 +45,126 @@ struct ManagerPeopleView: View {
                     }
                     
                     getManagerList(classCode: code) { managers in
-                            managerList = managers
-                        }
+                        managerList = managers
+                    }
                 }
         }else{
             VStack {
-                Text("People Enrolled in \(classTitle)")
+                Text("People in \(classTitle)")
                     .multilineTextAlignment(.center)
                     .font(.largeTitle)
                     .bold()
-                    .frame(width: 350, alignment: .center)
+                    .frame(width: 250, alignment: .center)
+                    .padding(.top, 40)
                 
-                Divider()
-                    .padding(20)
-                    .frame(width: 200)
-                
-                // managers displayed
-                Text("Managers")
-                    .multilineTextAlignment(.center)
-                    .font(.headline)
-                    .bold()
-                
-                List{
-                    if editing{
+                }
+            
+            .padding(.top, 20)
+
+        
+                VStack {
+                    Text("Managers")
+                        .multilineTextAlignment(.center)
+                        .font(.headline)
+                        .bold()
+                    
+                    List{
                         ForEach(managerList, id: \.self) { person in
-                            
-                            HStack {
-                                Text("\(person)")
-                                    .bold()
-//                      editing button code (add to owner for manager removal
-//                                Spacer()
-//                                Button(action: {
-//                                    unenrollClass(uid: person, code: code)
-//                                    removePersonFromClass(person: person, classCode: code)
-//                                    withAnimation {
-//                                        peopleList.removeAll(where: { $0 == person })
-//                                    }
-//                                }) {
-//                                    Image(systemName: "xmark.circle.fill")
-//                                        .foregroundColor(.red)
-//                                }
-                            }
-                        }.onDelete { indexSet in
-                            
-                            indexSet.forEach { index in
-                                let person = peopleList[index]
-                                removePersonFromClass(person: person, classCode: code)
-                            }
-                            
-                            withAnimation {
-                                peopleList.remove(atOffsets: indexSet)
-                            }
-                        }
-                    }else{
-                        ForEach(managerList, id: \.self) { person in
-                            
                             
                             HStack {
                                 Text("\(person)")
                                     .bold()
                                 Spacer()
-                      
+                                
                                 if let pfp = peopleToPfp[person]{
                                     Image(uiImage: pfp).resizable().clipShape(Circle()).frame(width: 30, height: 30).padding(.trailing).shadow(radius: 2.0,y:2.0)
                                 }else{
                                     Image(systemName: "person").resizable().clipShape(Circle()).frame(width: 30, height: 30).padding(.trailing).shadow(radius: 2.0,y:2.0)
-                                }
-                                
-                            }
-                        }
-                    }
-                }
-                
-                Divider()
-                    .padding(35)
-                    //.frame(width: 100)
-                
-                // students displayed
-                Text("Students")
-                    .multilineTextAlignment(.center)
-                    .font(.headline)
-                    .bold()
-                
-                VStack{
-                    HStack{
-                        Spacer()
-                        Button{
-                            editing.toggle()
-                        }label: {
-                            Image(systemName: editing ? "checkmark.rectangle.stack" : "square.and.pencil").resizable().frame(width: 20,height: 20).padding(.trailing, 20).padding(.bottom, 10).animation(.smooth(duration: 0.65), value: editing)
-                        }
-                    }
-                    List {
-                        if editing{
-                            ForEach(peopleList, id: \.self) { person in
-                                
-                                HStack {
-                                    Text("\(person)")
-                                        .bold()
-                                    Spacer()
-                                    Button(action: {
-                                        unenrollClass(uid: person, code: code)
-                                        removePersonFromClass(person: person, classCode: code)
-                                        withAnimation {
-                                            peopleList.removeAll(where: { $0 == person })
-                                        }
-                                    }) {
-                                        Image(systemName: "xmark.circle.fill")
-                                            .foregroundColor(.red)
-                                    }
-                                }
-                            }.onDelete { indexSet in
-                                
-                                indexSet.forEach { index in
-                                    let person = peopleList[index]
-                                    removePersonFromClass(person: person, classCode: code)
-                                }
-                                
-                                withAnimation {
-                                    peopleList.remove(atOffsets: indexSet)
-                                }
-                            }
-                        }else{
-                            ForEach(peopleList, id: \.self) { person in
-                                
-                                
-                                HStack {
-                                    Text("\(person)")
-                                        .bold()
-                                    Spacer()
-                          
-                                    if let pfp = peopleToPfp[person]{
-                                        Image(uiImage: pfp).resizable().clipShape(Circle()).frame(width: 30, height: 30).padding(.trailing).shadow(radius: 2.0,y:2.0)
-                                    }else{
-                                        Image(systemName: "person").resizable().clipShape(Circle()).frame(width: 30, height: 30).padding(.trailing).shadow(radius: 2.0,y:2.0)
-                                    }
+                                    
                                     
                                 }
                             }
+
                         }
                     }
                 }
-            }
-            .padding(.top, 20)
             
+                .padding(.top, 10)
+                    
+                    
+
+
+                
+                    // students displayed
+                    Text("Students")
+                        .multilineTextAlignment(.center)
+                        .font(.headline)
+                        .bold()
+                    
+                    VStack{
+                        HStack{
+                            Spacer()
+                            Button{
+                                editing.toggle()
+                            }label: {
+                                Image(systemName: editing ? "checkmark.rectangle.stack" : "square.and.pencil").resizable().frame(width: 20,height: 20).padding(.trailing, 20).padding(.bottom, 10).animation(.smooth(duration: 0.65), value: editing)
+                            }
+                        }
+                        List {
+                            if editing{
+                                ForEach(peopleList, id: \.self) { person in
+                                    
+                                    HStack {
+                                        Text("\(person)")
+                                            .bold()
+                                        Spacer()
+                                        Button(action: {
+                                            unenrollClass(uid: person, code: code)
+                                            removePersonFromClass(person: person, classCode: code)
+                                            withAnimation {
+                                                peopleList.removeAll(where: { $0 == person })
+                                            }
+                                        }) {
+                                            Image(systemName: "xmark.circle.fill")
+                                                .foregroundColor(.red)
+                                        }
+                                    }
+                                }.onDelete { indexSet in
+                                    
+                                    indexSet.forEach { index in
+                                        let person = peopleList[index]
+                                        removePersonFromClass(person: person, classCode: code)
+                                    }
+                                    
+                                    withAnimation {
+                                        peopleList.remove(atOffsets: indexSet)
+                                    }
+                                }
+                            }else{
+                                ForEach(peopleList, id: \.self) { person in
+                                    
+                                    
+                                    HStack {
+                                        Text("\(person)")
+                                            .bold()
+                                        Spacer()
+                                        
+                                        if let pfp = peopleToPfp[person]{
+                                            Image(uiImage: pfp).resizable().clipShape(Circle()).frame(width: 30, height: 30).padding(.trailing).shadow(radius: 2.0,y:2.0)
+                                        }else{
+                                            Image(systemName: "person").resizable().clipShape(Circle()).frame(width: 30, height: 30).padding(.trailing).shadow(radius: 2.0,y:2.0)
+                                        
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            // end of V stack below "Students" text
+                .padding(.top, 10)
+                
+            }
         }
     }
-}
+
