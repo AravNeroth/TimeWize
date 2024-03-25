bc79dbb17b0cd8d96006528c9fcdd75a14b38398
1J6K21 <Jonathan.kalsky@gmail.com>
2024-03-25 15:35:21 -0500

    hourboardonprofile

--- ServiceHourTracker/BasicViews/Profile.swift
+++ ServiceHourTracker/BasicViews/Profile.swift
@@ -88,48 +88,16 @@ struct Profile: View {
                     
                 
                 )
-            /*
-            VStack(alignment: .leading){
-                Spacer()
-                
-                HStack(alignment: .bottom){
-                    ZStack{
-                        Circle()
-                            .foregroundStyle(.black)
-                            .frame(width: 80, height: 80)
-                        
-                            Image(.image1)
-                                .frame(width: 75,height: 75)
-                                .scaledToFit()
-                                .clipShape(Circle())
-                                .overlay(
-                            
-                                    VStack{
-                                        Spacer()
-                                        HStack(alignment: .bottom){
-                                            Spacer()
-                                            ZStack{
-                                                Circle().frame(width: 25,height: 25).foregroundStyle(.black)
-                                                Image(systemName: "pencil").foregroundStyle(.white)
-                                            }
-                                        }
-                                        
-                                    }
-                            )
-                            
-                        
-                        
-                    }.padding(.leading, 10)
-                    Spacer()
-                }
-                
-            }
-            */
             
-        }.ignoresSafeArea().frame(height: 225).position(x: UIScreen.main.bounds.width / 2, y: 75)
+            
+        }.ignoresSafeArea().frame(height: 225).position(x: UIScreen.main.bounds.width / 2, y: 75).frame(height: 190)
         
         
     ScrollView{
+    
+         
+                HourBoardView()
+            
         
     }
     
