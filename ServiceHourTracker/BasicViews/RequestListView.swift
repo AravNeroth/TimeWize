b057dad52e74249e51012d25f6e7ae5fb1c72567
AravNeroth <reacharav@outlook.com>
2024-04-16 11:59:09 -0500

    ACTION TAB IS NOT COOKED IT WORKS

--- ServiceHourTracker/BasicViews/RequestListView.swift
+++ ServiceHourTracker/BasicViews/RequestListView.swift
@@ -11,45 +11,71 @@ struct RequestListView: View {
     
     @State var colorsForRequest: [Request:[Color]] = [:]
     @State var classForRequest: [Request:Classroom] = [:]
-    @State var fromManSide = true
+    @State var fromManSide = false
     @State var checkAccepted = false
-    @State var requestList: [Request] = []
+    @State var pendingRequests: [Request] = []
+    @State var acceptedRequests: [Request] = []
     @State var done = false
     @AppStorage("uid") var userID = ""
     
     var body: some View {
         if done {
             ScrollView {
-                if !requestList.isEmpty {
-                    ForEach(requestList) { request in
+                // * i dunno how to make the boxes fit
+                
+                
+                // check if there is any pending
+                if pendingRequests.isEmpty {
+                    Text("No Pending Requests").bold()
+                }
+                else{
+                    Text("PENDING REQUESTS").bold()
+                    
+                // if there is pending req, display
+                    ForEach(pendingRequests) { request in
                         NewRequestView(className: classForRequest[request]!.title, classCode: request.classCode, colors: colorsForRequest[request]!, description: request.description, numHours: request.numHours, hourType: request.hourType, email: request.creator, request: request, fromManSide: fromManSide, done: $done)
                     }
-                } else {
-                    if fromManSide {
-                        Text("No Requests")
-                    } else {
-                        Text("No Requests Pending")
-                    }
+
                 }
+                
+                    Divider() // do yuo use spacer or divider or both for this?
+                    
+                
+                // check if there is any accepted
+                if acceptedRequests.isEmpty {
+                    Text("No Accepted Requests").bold()
+                
+                }
+                else{
+                    Text("ACCEPTED REQUESTS").bold()
+                    // if there is accepted req, display
+                    ForEach(acceptedRequests) { request in
+                        NewRequestView(className: classForRequest[request]!.title, classCode: request.classCode, colors: colorsForRequest[request]!, description: request.description, numHours: request.numHours, hourType: request.hourType, email: request.creator, request: request, fromManSide: fromManSide, done: $done)
+                    }
+
+                }
+                        
             }
-        } else {
+        } // end of "done" / end of view
+        
+         else {
             LoadingScreen()
                 .padding()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(ignoresSafeAreaEdges: .all)
                 .onAppear() {
-                    requestList = []
+                    pendingRequests = []
+                    acceptedRequests = []
                     colorsForRequest = [:]
                     classForRequest = [:]
                     
-                    print("fromManSide: \(fromManSide)")
-                    
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-                        if fromManSide {
+                        
+                        if fromManSide { // if user is manager
                             getClasses(uid: userID) { classes in
                                 if let classes = classes {
                                     for classCode in classes {
-                                        getClassRequests(classCode: classCode) { requests in
+                                        getClassRequests(classCode: classCode) { requests in // below gets all requests in class
                                             for request in requests {
                                                 getColorScheme(classCode: classCode) { colors in
                                                     colorsForRequest[request] = colors
@@ -59,16 +85,17 @@ struct RequestListView: View {
                                                         classForRequest[request] = classroom
                                                     }
                                                 }
-                                                requestList.append(request)
+                                                pendingRequests.append(request)
                                             }
                                         }
                                     }
                                 }
                             }
-                        } else {
-                            if !checkAccepted {
-                                getPendingRequests(email: userID) { requests in
-                                    print(requests)
+                        } // end of manager (note* - we need a func that gets logbook of all past accepted requests!
+                        
+                        else { // if user is student
+                            
+                                getPendingRequests(email: userID) { requests in // gets pending
                                     for request in requests {
                                         getColorScheme(classCode: request.classCode) { colors in
                                             colorsForRequest[request] = colors
@@ -78,13 +105,10 @@ struct RequestListView: View {
                                                 classForRequest[request] = classroom
                                             }
                                         }
-                                        requestList.append(request)
+                                        pendingRequests.append(request) // finds all pending req & appends to pendingRequest to be displayed
                                     }
-                                }
-                            } 
-                            else {
-                                getAcceptedRequests(email: userID) { requests in
-                                    print(requests)
+                                    
+                                getAcceptedRequests(email: userID) { requests in // gets accepted
                                     for request in requests {
                                         getColorScheme(classCode: request.classCode) { colors in
                                             colorsForRequest[request] = colors
@@ -94,15 +118,18 @@ struct RequestListView: View {
                                                 classForRequest[request] = classroom
                                             }
                                         }
-                                        requestList.append(request)
+                                        acceptedRequests.append(request) // finds all accepted req & appends to pendingRequest to be displayed
                                     }
                                 }
                             }
-                        }
+                        } // end of from student
+                        
+                        
                     }
                     
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
-                        requestList.sort { $0.timeCreated < $1.timeCreated }
+                        pendingRequests.sort { $0.timeCreated < $1.timeCreated }
+                        acceptedRequests.sort { $0.timeCreated < $1.timeCreated }
                         done = true
                     }
                 }
